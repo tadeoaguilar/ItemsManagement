88e7a12c46968fcf34097dd1690d584f7a555911
tadeoaguilar <tadeo.am@gmail.com>
2025-04-29 21:51:30 -0400

    Images handling1

--- src/modules/eItems.Identity/Data/IdentityContext.cs
+++ src/modules/eItems.Identity/Data/IdentityContext.cs
@@ -3,6 +3,8 @@ using eItems.Identity.Data.Model;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+using Microsoft.EntityFrameworkCore.Design;
+
 namespace eItems.Identity.Data;
 
 public class IdentityAppDbContext : IdentityDbContext<ApplicationUser>
@@ -11,14 +13,26 @@ public class IdentityAppDbContext : IdentityDbContext<ApplicationUser>
         : base(options)
     {}
 
-         protected override void OnModelCreating(ModelBuilder builder)
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
             //builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-            builder.HasDefaultSchema("identity");
+           
             
 
             base.OnModelCreating(builder);
-
+            builder.HasDefaultSchema("identity");
         }
     
 }
+/*
+public class IdentityContextFactory : IDesignTimeDbContextFactory<IdentityAppDbContext>
+{
+    public IdentityAppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<IdentityAppDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost:5432;Database=eItems;Username=postgres;Password=C~u3zS1rB429CK(EkrvwbW");
+
+        return new IdentityAppDbContext(optionsBuilder.Options);
+    }
+}*/
